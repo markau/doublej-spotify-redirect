b84931140c31a2ead86628eb9dc2dd4389336d2b
Mark Andrews <mark@coolsmallapps.com>
2021-07-25 11:40:28 +0930

    Added styles

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const songName = document.querySelector(".song-name");
 const artistName = document.querySelector(".artist-name");
 const artwork = document.querySelector(".artwork");
 const spotifyLink = document.querySelector(".spotify-link");
+const blockElement = document.querySelector(".block-element");
 
 const timeBuffer = 5;
 const timeZoneOffsetMinutes = 30;
@@ -19,12 +20,16 @@ const utcPlus = new Date(dateObj.getTime() - minutesPlus * 60000).toISOString();
 // const utcPlus = new Date(dateObj.getTime() - 25 * 60000).toISOString();
 const api = `https://music.abcradio.net.au/api/v1/plays/search.json?station=doublej&from=${utcMinus}&to=${utcPlus}&limit=4&order=desc`;
 
+songName.classList.add('text-transition');
+artistName.classList.add('text-transition');
+artwork.classList.add('artwork-transition');
+blockElement.classList.add('text-transition');
 
 fetch(api)
   .then(response => response.json())
   .then(data => {
     const recentPlays = data?.items;
-    loading.style.display = "none";
+    // loading.style.display = "none";
 
     const utcExact = new Date(dateObj.getTime() - timeZoneOffsetMinutes * 60000).toISOString();
     const song = recentPlays.find((song) => song.played_time <= utcExact);
@@ -42,11 +47,19 @@ fetch(api)
     if (song) {
       const songTitle = song?.recording?.title;
       const songArtist = song?.recording?.artists[0]?.name;
-      const songArtwork = song?.release?.artwork[0]?.url;
+      const songArtwork = song?.release?.artwork[0]?.url || "https://www.abc.net.au/assets/radio-2.53/images/icons/album.png";
       songName.textContent = songTitle;
       artistName.textContent = songArtist;
       spotifyLink.setAttribute('href', `https://play.spotify.com/search/results/artist:${songArtist} track:${songTitle}`);
+      spotifyLink.textContent = "Spotify";
+      // artwork.setAttribute('src', songArtwork).remove('artwork-transition');
       artwork.setAttribute('src', songArtwork);
+
+      songName.classList.remove('text-transition');
+      artistName.classList.remove('text-transition');
+      blockElement.classList.remove('text-transition');
+      artwork.classList.remove('artwork-transition');
+      artwork.classList.remove('display-none');
     }
 
   })
